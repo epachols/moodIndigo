f773f20cd8853f1be09be576fbd4e188f34a15ec
wolfdog27 <kylenicholasanderson@gmail.com>
2020-06-24 14:50:20 -0700

    finished intial 3 happy and sad arrays

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 // // the following is just a test fire of js link
 // $("body").prepend($("<p>").text("HOWDY"));
+
+var happy = [{
+    "name": "happyBeats"
+    "uri": "37i9dQZF1DWSf2RDTDayIx"
+},
+{
+    "name": "happyFolk"
+    "uri": "37i9dQZF1DWSkMjlBZAZ07"
+},
+{
+    "name": "happyThrowback"
+    "uri": "37i9dQZF1DWVOMXLzSabIM"
+}]
+
+var sad = [{
+    "name": "sadCry"
+    "uri": "7ABD15iASBIpPP5uJ5awvq"
+},
+{
+    "name": "sadClassical"
+    "uri": "37i9dQZF1DXbm0dp7JzNeL"
+},
+{
+    "name": "sadDepression"
+    "uri": "37i2v94FtSr0YRmW9LetICu4q"
+}]
+
+
+
+// need array for emotions
+// need to make general function that acccepts happy array or sad array
+// randomized the contents of that array
+// pick the first one
+// plugs in its uri to iframe URL
+// generate iframe via uri plugged in in space selected
